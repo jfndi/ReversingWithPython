4ced202ab25cfcb4e6e6167975f0af639212360c
jfndi <143571177+jfndi@users.noreply.github.com>
2024-01-25 09:20:36 +0100

    Modified pe_defines. IMAGE_SECTION_HEADER added.

--- PE/pe_defines.py
+++ PE/pe_defines.py
@@ -101,6 +101,10 @@ IMAGE_SUBSYSTEM_XBOX_CODE_CATALOG           = 17
 #
 # Dll characteristics.
 #
+IMAGE_LIBRARY_PROCESS_INIT                      = 0x0001
+IMAGE_LIBRARY_PROCESS_TERM                      = 0x0002
+IMAGE_LIBRARY_THREAD_INIT                       = 0x0004
+IMAGE_LIBRARY_THREAD_TERM                       = 0x0008
 IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA        = 0x0020  
 IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE           = 0x0040     
 IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY        = 0x0080     
@@ -130,9 +134,60 @@ IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG       = 10
 IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT      = 11   
 IMAGE_DIRECTORY_ENTRY_IAT               = 12   
 IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT      = 13   
-IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR    = 14   
+IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR    = 14
+
+#
+# Section characteristics.
+#
+IMAGE_SCN_TYPE_REG                  = 0x00000000 
+IMAGE_SCN_TYPE_DSECT                = 0x00000001 
+IMAGE_SCN_TYPE_NOLOAD               = 0x00000002 
+IMAGE_SCN_TYPE_GROUP                = 0x00000004 
+IMAGE_SCN_TYPE_NO_PAD               = 0x00000008 
+IMAGE_SCN_TYPE_COPY                 = 0x00000010 
+IMAGE_SCN_CNT_CODE                  = 0x00000020 
+IMAGE_SCN_CNT_INITIALIZED_DATA      = 0x00000040 
+IMAGE_SCN_CNT_UNINITIALIZED_DATA    = 0x00000080 
+IMAGE_SCN_LNK_OTHER                 = 0x00000100 
+IMAGE_SCN_LNK_INFO                  = 0x00000200 
+IMAGE_SCN_TYPE_OVER                 = 0x00000400 
+IMAGE_SCN_LNK_REMOVE                = 0x00000800 
+IMAGE_SCN_LNK_COMDAT                = 0x00001000 
+IMAGE_SCN_MEM_PROTECTED - Obsolete  = 0x00004000
+IMAGE_SCN_NO_DEFER_SPEC_EXC         = 0x00004000 
+IMAGE_SCN_GPREL                     = 0x00008000 
+IMAGE_SCN_MEM_FARDATA               = 0x00008000
+IMAGE_SCN_MEM_PURGEABLE             = 0x00020000
+IMAGE_SCN_MEM_16BIT                 = 0x00020000
+IMAGE_SCN_MEM_LOCKED                = 0x00040000
+IMAGE_SCN_MEM_PRELOAD               = 0x00080000
+IMAGE_SCN_ALIGN_1BYTES              = 0x00100000
+IMAGE_SCN_ALIGN_2BYTES              = 0x00200000
+IMAGE_SCN_ALIGN_4BYTES              = 0x00300000
+IMAGE_SCN_ALIGN_8BYTES              = 0x00400000
+IMAGE_SCN_ALIGN_16BYTES             = 0x00500000 
+IMAGE_SCN_ALIGN_32BYTES             = 0x00600000
+IMAGE_SCN_ALIGN_64BYTES             = 0x00700000
+IMAGE_SCN_ALIGN_128BYTES            = 0x00800000
+IMAGE_SCN_ALIGN_256BYTES            = 0x00900000
+IMAGE_SCN_ALIGN_512BYTES            = 0x00A00000
+IMAGE_SCN_ALIGN_1024BYTES           = 0x00B00000
+IMAGE_SCN_ALIGN_2048BYTES           = 0x00C00000
+IMAGE_SCN_ALIGN_4096BYTES           = 0x00D00000
+IMAGE_SCN_ALIGN_8192BYTES           = 0x00E00000
+IMAGE_SCN_ALIGN_MASK                = 0x00F00000
+IMAGE_SCN_LNK_NRELOC_OVFL           = 0x01000000 
+IMAGE_SCN_MEM_DISCARDABLE           = 0x02000000 
+IMAGE_SCN_MEM_NOT_CACHED            = 0x04000000 
+IMAGE_SCN_MEM_NOT_PAGED             = 0x08000000 
+IMAGE_SCN_MEM_SHARED                = 0x10000000 
+IMAGE_SCN_MEM_EXECUTE               = 0x20000000 
+IMAGE_SCN_MEM_READ                  = 0x40000000 
+IMAGE_SCN_MEM_WRITE                 = 0x80000000 
+ 
 
 class IMAGE_DOS_HEADER(ct.Structure):
+    _pack_ = 2
     _fields_ = [
         ("e_magic",         wt.WORD),
         ("e_cblp",          wt.WORD),
@@ -283,3 +338,25 @@ class IMAGE_ROM_HEADERS32(ct.Structure):
         ("FileHeader",      IMAGE_FILE_HEADER),
         ("OptionalHeader",  IMAGE_ROM_OPTIONAL_HEADER),
         ]
+
+class ANONUNION(ct.Union):
+    _fields_ = [
+        ("PhysicalAddress",     ct.DWORD),
+        ("VirtualSize",         ct.DWORD),
+        ]
+
+IMAGE_SIZEOF_SHORT_NAME       = 8
+
+class IMAGE_SECTION_HEADER(ct.Structure):
+    _fields_ = [
+        ("Misc",                    wt.BYTE * IMAGE_SIZEOF_SHORT_NAME),
+        ("VirtualAddress",          wt.DWORD),
+        ("SizeOfRawData",           wt.DWORD),
+        ("PointerToRawData",        wt.DWORD),
+        ("PointerToLineNumbers",    wt.WORD),
+        ("NumberOfRelocations",     wt.WORD),
+        ("NumberOfLineNumbers",     wt.WORD),
+        ("Characteristics",         wt.DWORD),
+        ]
+
+IMAGE_SIZEOF_SECTION_HEADER     = 40
